67b5d24a28832596aea666e556d3ee4597cd9556
tandatlu <lutandat2018@gmail.com>
2023-02-18 23:36:04 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,17 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss'; // mac dinh style ko cho viet dau gach ngang
+
+const cx = classNames.bind(styles); // cho phep style viet co dau gach ngang
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/*logo*/}
+                {/*logo*/}
+            </div>
+        </header>
+    );
 }
 
 export default Header;
